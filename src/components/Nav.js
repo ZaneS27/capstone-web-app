5ba8e22eb168801325addd63ea5eadb1fe5c6527
Zane SKwarlo <zane.skwarlo@gmail.com>
2024-01-03 20:01:41 -0600

    Added Assets and Starter Nav

---
+++ src/components/Nav.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import logo from '../images/Logo .svg'
+
+const Nav = () => {
+    return (
+        <nav>
+            <a href="/">
+            <img src={logo}/>
+            </a>
+            <div>
+                <div className='bar'></div>
+                <div className='bar'></div>
+                <div className='bar'></div>
+            </div>
+
+            <ul>
+                <li>
+                <a href="/">Home</a>
+                </li> 
+                <li>
+                <a href="/">About</a>
+                </li> 
+                <li>
+                <a href="/">Services</a>
+                </li> 
+                <li>
+                <a href="/">Menu</a>
+                </li> 
+                <li>
+                <a href="/">Reservations</a>
+                </li> 
+                <li>
+                <a href="/">Order Online</a>
+                </li> 
+                <li>
+                <a href="/">Login</a>
+                </li> 
+            </ul>
+
+        </nav>
+    );
+};
+
+export default Nav;
